744713fa8c9ab9141a3502d9fdb6c2477da58658
Koichi Murase <myoga.murase@gmail.com>
2016-03-26 20:50:59 +0900

    adapter/gcc/gcc2-cxx.sh (-MD): correctly search the default *.d output.

--- share/adapter/gcc/gcc2-cxx.sh
+++ share/adapter/gcc/gcc2-cxx.sh
@@ -7,28 +7,30 @@ mcxx_bash=$((${BASH_VERSINFO[0]}*10000+${BASH_VERSINFO[1]}*100+${BASH_VERSINFO[2
 
 args=()
 if ((mcxx_bash>=30100)); then
-  function args.push { args+=("$@"); }
+  function array.push { eval "$1+=(\"\${@:2}\")"; }
 else
-  function args.push { args=("${args[@]}" "$@"); }
+  function array.push { eval "$1+=(\"\${$1[@]}\" \"\${@:2}\")"; }
 fi
 
 arg_input=
 function process-input {
   [[ -f $1 && ! $arg_input ]] && arg_input="$1"
-  args.push "$1"
+  array.push args "$1"
 }
 
 arg_compile=
 function process-option:-c {
   arg_compile=1
-  args.push -c
+  array.push args -c
 }
 
 arg_output=
 function process-option:-o {
   arg_output="$1"
-  args.push -o "$1"
+  array.push args -o "$1"
 }
+
+dep_enabled=
 
 # -MT filename/-MQ filename は gcc-2.95 では使えない様だ。
 dep_target_sed=
@@ -65,11 +67,14 @@ function read-arguments {
     (-MT*) process-option:-MT "${arg#-MT}" ;;
     (-MQ)  process-option:-MQ "$1"; shift ;;
     (-MQ*) process-option:-MQ "${arg#-MQ}" ;;
+    (-MD|-MMD)
+      dep_enabled=1
+      args.push args "$args" ;;
     (-[ILDUulxAzB]|-Xlinker|-Xpreprocessor)
-      args.push "$arg" "$1"
-      shift 1 ;;
+      array.push args "$arg" "$1"
+      shift ;;
     (-c)   process-option:-c ;;
-    (-*)   args.push "$arg" ;;
+    (-*)   array.push args "$arg" ;;
     (*)    process-input "$arg" ;;
     esac
   done
@@ -95,29 +100,48 @@ if [[ ! $arg_output ]]; then
 fi
 
 # 依存関係 *.d の予想位置
-if [[ $arg_output == *.o ]]; then
-  dep_output_tmp="${arg_output%.o}.d"
-elif [[ -f $arg_input ]]; then
-  dep_output_tmp="${arg_input%.*}.d"
-else
-  dep_output_tmp="a.d"
-fi
-if [[ -f ${dep_output_tmp##*/} ]]; then
-  # 実は current directory にできる?
-  dep_output_tmp="${dep_output_tmp##*/}"
+function find-dep_output_tmp {
+  dep_output_tmp=
+
+  local -a cand=()
+
+  if [[ -f $arg_input ]]; then
+    local c="${arg_input%.*}.d"
+    array.push cand "./${c##*/}" "$c"
+  else
+    dep_output_tmp="./-.d"
+  fi
+
+  if [[ $arg_output == *.o ]]; then
+    local c="${arg_output%.o}.d"
+    array.push cand "./${c##*/}" "$c"
+  fi
+
+  local c
+  for c in "${cand[@]}"; do
+    if [[ -f $c && ( ! $dep_output_tmp || $c -nt $dep_output_tmp ) ]]; then
+      dep_output_tmp="$c"
+    fi
+  done
+}
+
+if [[ $dep_enabled ]]; then
+  dep_output_tmp=
+  find-dep_output_tmp
+
+  # -MF -MT の処理
+  if [[ -f $dep_output_tmp ]]; then
+    if [[ $dep_output_set && $dep_output_tmp != $dep_output ]]; then
+      mv -f "$dep_output_tmp" "$dep_output"
+    else
+      dep_output="$dep_output_tmp"
+    fi
+    if [[ $dep_target_sed ]]; then
+      sed -e "$dep_target_sed" "$dep_output" > "$dep_output.tmp" &&
+        mv -- "$dep_output.tmp" "$dep_output"
+    fi
+  fi
 fi
 
-# -MF -MT の処理
-if [[ -f $dep_output_tmp ]]; then
-  if [[ $dep_output_set && $dep_output_tmp != $dep_output ]]; then
-    mv -f "$dep_output_tmp" "$dep_output"
-  else
-    dep_output="$dep_output_tmp"
-  fi
-  if [[ $dep_target_sed ]]; then
-    sed -e "$dep_target_sed" "$dep_output" > "$dep_output.tmp" &&
-      mv -- "$dep_output.tmp" "$dep_output"
-  fi
-fi
 
 exit "$exit"
